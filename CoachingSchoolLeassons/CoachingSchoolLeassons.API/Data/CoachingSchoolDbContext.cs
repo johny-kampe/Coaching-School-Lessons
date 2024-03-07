d0edca568c2ec0c041abdf22a66bd84d1c7a279c
John Kamperakis <johnkamperakis@gmail.com>
2024-03-07 16:06:29 +0100

    Initialized the API project, impleneting the DbContext in order to use EF

---
+++ CoachingSchoolLeassons/CoachingSchoolLeassons.API/Data/CoachingSchoolDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CoachingSchoolLeassons.API.Models.Domain;
+
+namespace CoachingSchoolLeassons.API.Data
+{
+    public class CoachingSchoolDbContext : DbContext
+    {
+        public CoachingSchoolDbContext(DbContextOptions<CoachingSchoolDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Teacher> Teachers { get; set; }
+    }
+}
---
+++ CoachingSchoolLeassons/CoachingSchoolLeassons.API/Models/Domain/Teacher.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CoachingSchoolLeassons.API.Models.Domain
+{
+    public class Teacher
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
